85189d4f53bc5e3024849bf43dbb5ec32715d0df
abhishek24031996 <abhishekjat376@gmail.com>
2024-05-18 15:42:35 +0530

    front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DemoAngularMaterialModule } from './DemoAngularMaterialModule';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
+import { FormsModule } from "@angular/forms";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DemoAngularMaterialModule,
+    BrowserModule, 
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    HttpClientXsrfModule,
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
